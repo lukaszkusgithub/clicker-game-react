2533a25d444c2a940a56c9ff06baf622ff8276ef
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2024-12-04 20:32:17 +0100

    feat: create basic interface and functionality

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import GoldClicker from "./components/GoldClicler";
+
+function App() {
+	return (
+		<>
+			<GoldClicker />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/GoldClicler.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+function GoldClicker() {
+	const [gold, setGold] = useState(0);
+	const [clickPower, setClickPower] = useState(1);
+	const [clickUpgradeCost, setClickUpgradeCost] = useState(10);
+
+	const upgradeClickPower = () => {
+		if (gold >= clickUpgradeCost) {
+			setGold(gold - clickUpgradeCost);
+			setClickPower(clickPower + 1);
+			setClickUpgradeCost(clickUpgradeCost * 2);
+		}
+	};
+
+	return (
+		<div>
+			<h1>Gold Clicker</h1>
+			<p>Gold: {gold}</p>
+			<p>Click power: {clickPower}</p>
+			<button onClick={() => setGold(gold + clickPower)}>Dig gold</button>
+			<button
+				disabled={gold < clickUpgradeCost}
+				onClick={upgradeClickPower}>
+				Upgrade click power (Cost: {clickUpgradeCost})
+			</button>
+		</div>
+	);
+}
+
+export default GoldClicker;
