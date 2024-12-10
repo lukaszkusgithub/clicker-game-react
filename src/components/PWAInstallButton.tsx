cfb8a85469ff787000269edf779ab616f734c891
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2024-12-10 22:42:04 +0100

    Add install button functionality

--- src/components/PWAInstallButton.tsx
+++ src/components/PWAInstallButton.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
 
+interface BeforeInstallPromptEvent extends Event {
+	readonly platform: string;
+	readonly userChoice: Promise<{ outcome: string; platform: string }>;
+	prompt(): Promise<{ outcome: string; platform: string }>;
+}
+
 function PWAInstallButton() {
 	const [showInstallButton, setShowInstallButton] = useState(false);
+	const [deferredPrompt, setDeferredPrompt] =
+		useState<BeforeInstallPromptEvent | null>(null);
 
 	useEffect(() => {
-		const handleBeforeInstallPrompt = (event: {
-			preventDefault: () => void;
-		}) => {
-			event.preventDefault();
+		const handleBeforeInstallPrompt = (event: Event) => {
+			const beforeInstallEvent = event as BeforeInstallPromptEvent;
+			beforeInstallEvent.preventDefault();
+			setDeferredPrompt(beforeInstallEvent);
 			setShowInstallButton(true);
 		};
 		window.addEventListener(
@@ -21,7 +29,20 @@ function PWAInstallButton() {
 			);
 	}, []);
 
-	return showInstallButton ? <button>Install PWA app</button> : null;
+	const handleInstallClick = async () => {
+		if (deferredPrompt) {
+			const userChoice = await deferredPrompt.prompt();
+
+			if (userChoice.outcome === "accepted") {
+				setDeferredPrompt(null);
+				setShowInstallButton(false);
+			}
+		}
+	};
+
+	return showInstallButton ? (
+		<button onClick={handleInstallClick}>Install PWA app</button>
+	) : null;
 }
 
 export default PWAInstallButton;
