973c0a4866db0fb3b108230fe03e90684aaffcde
Luryxyt <36661311+lukaszkusgithub@users.noreply.github.com>
2024-12-18 13:02:50 +0100

    Fix code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 5050;
+
+console.log(`Server is running on port ${PORT}`);
 
 app.get("/", (_req, res) => {
 	res.send("Hello World!");
 });
 
-app.listen(port);
+app.listen(PORT, () => {
+	console.log(`Server is listening on port ${PORT}`);
+});
